2d70dc80b43fb76c13ed93299657542f49f74c20
Khalid <khalidhasantalukder5@gmail.com>
2025-08-17 12:05:20 +0600

    3rd

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 // index.php
-echo "Hello, World! Welcome to CI/CD testing in PHP!v 
+echo "Hello, World! Welcome to CI/CD testing in PHP!";
+echo "<br>";
 echo "This is a simple PHP application for testing purposes.";
 ?>
