5ba3775bfd6c892f6b20cb4d1b9ef013f5f7ad63
Khalid <khalidhasantalukder5@gmail.com>
2025-08-17 12:02:57 +0600

    add test

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 // index.php
-echo "Hello, World! Welcome to CI/CD testing in PHP!";
+echo "Hello, World! Welcome to CI/CD testing in PHP!v 
+echo "This is a simple PHP application for testing purposes.";
 ?>
